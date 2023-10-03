d6114fc78d29059871816e797a2b68478d3c5845
adnan-shaikh23242324 <107952487+adnan-shaikh23242324@users.noreply.github.com>
2023-10-03 19:46:12 +0530

    update delete

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dbConnect =require('./mongodb');
+const mongodb = require('mongodb');
  const app = express();
- 
+ app.use(express.json());
  
  app.get('',async(req,res)=>{
     let data = await dbConnect();
@@ -12,4 +13,29 @@ const dbConnect =require('./mongodb');
 
  });
 
- app.listen(4500)
+ app.post('',async(req,res)=>{
+    let data = await dbConnect();
+    result = await data.insertOne(req.body);
+    res.send(result);
+ })
+
+ app.put("/",async(req,res)=>{
+   let data = await dbConnect();
+   let result =data.updateOne(
+      {name:"req.body.name"},
+      {$set:req.body}
+      )
+      res.send({result:"update"})
+   // console.log(req.body);
+   // res.send({result:"update"});
+})
+
+app.delete("/:id",async(req,res)=>{
+      console.log(req.params.id);
+      const data = await dbConnect();
+      const result =data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+
+      res.send(result);
+   })
+
+ app.listen(4500);
